ca5ebb227c4ac058b09d1c8b6cbd9189cb7a1f9d
heshanu <heshanu97@gmail.com>
2024-11-05 21:14:03 +0530

    firstComment

---
+++ src/main/java/com/example/AssignmentSubmissonApp/AssignmentSubmissonAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.AssignmentSubmissonApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentSubmissonAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentSubmissonAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AssignmentSubmissonApp/AssignmentSubmissonAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AssignmentSubmissonApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentSubmissonAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
